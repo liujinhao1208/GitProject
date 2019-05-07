4d1e950cdb88a280ac19d3c871479c9abfeb8142
liujinhao1208 <49189556+liujinhao1208@users.noreply.github.com>
2019-05-07 17:43:39 +0800

    Update StudentModel.java

--- student_pro/src/com/student/model/StudentModel.java
+++ student_pro/src/com/student/model/StudentModel.java
@@ -5,7 +5,7 @@ public class StudentModel {
 	private String username;//用户名
 	private String password;//密码
 	private int sex;//性别
-	private String love;//爱
+	private String love;//爱好
 	
 	public StudentModel(int id, String username, String password, int sex, String love) {
 		super();
